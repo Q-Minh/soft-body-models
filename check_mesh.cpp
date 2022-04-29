f59ffb166d076cc6858b20f91936f90e420c124d
Quoc-Minh <tonthat.quocminh@gmail.com>
2022-04-29 09:48:59 -0400

    Tools for extracting surface from volume and checking mesh

---
+++ check_mesh.cpp
@@ -0,0 +1,135 @@
+#include <igl/readPLY.h>
+#include <igl/readOBJ.h>
+#include <igl/readSTL.h>
+#include <igl/is_vertex_manifold.h>
+#include <igl/is_edge_manifold.h>
+#include <igl/boundary_loop.h>
+
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/Polyhedron_items_with_id_3.h>
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+/**
+ * @brief
+ * Taken from https://github.com/libigl/libigl/blob/main/include/igl/copyleft/cgal/mesh_to_polyhedron.cpp .
+ * We add the verbose flag to the polyhedron incremental builder to get some useful output.
+ *
+ * @tparam DerivedV
+ * @tparam DerivedF
+ * @tparam Polyhedron
+ * @param V
+ * @param F
+ * @param poly
+ * @return true
+ * @return false
+ */
+template <
+    typename DerivedV,
+    typename DerivedF,
+    typename Polyhedron>
+bool mesh_to_polyhedron(
+    const Eigen::MatrixBase<DerivedV> &V,
+    const Eigen::MatrixBase<DerivedF> &F,
+    Polyhedron &poly)
+{
+    typedef typename Polyhedron::HalfedgeDS HalfedgeDS;
+    // Postcondition: hds is a valid polyhedral surface.
+    CGAL::Polyhedron_incremental_builder_3<HalfedgeDS> B(poly.hds(), true);
+    B.begin_surface(V.rows(), F.rows());
+    typedef typename HalfedgeDS::Vertex Vertex;
+    typedef typename Vertex::Point Point;
+    assert(V.cols() == 3 && "V must be #V by 3");
+    for (int v = 0; v < V.rows(); v++)
+    {
+        B.add_vertex(Point(V(v, 0), V(v, 1), V(v, 2)));
+    }
+    assert(F.cols() == 3 && "F must be #F by 3");
+    for (int f = 0; f < F.rows(); f++)
+    {
+        B.begin_facet();
+        for (int c = 0; c < 3; c++)
+        {
+            B.add_vertex_to_facet(F(f, c));
+        }
+        B.end_facet();
+    }
+    if (B.error())
+    {
+        B.rollback();
+        return false;
+    }
+    B.end_surface();
+    return poly.is_valid();
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1 || argc != 2)
+    {
+        std::cout << "Usage:\n";
+        std::cout << "./check_mesh.exe input.[ply|obj]\n";
+        return 0;
+    }
+
+    std::filesystem::path const input{argv[1]};
+    std::string const input_extension = input.extension().string();
+    bool const is_valid_input_file =
+        std::filesystem::exists(input) &&
+        std::filesystem::is_regular_file(input) &&
+        (input_extension == ".ply" ||
+         input_extension == ".obj" ||
+         input_extension == ".stl");
+
+    if (!is_valid_input_file)
+    {
+        std::cerr << "Expected input to be .[ply|obj|stl]\n";
+        return 1;
+    }
+
+    Eigen::MatrixXd V;
+    Eigen::MatrixXi F;
+
+    if (input_extension == ".ply")
+    {
+        igl::readPLY(input.string(), V, F);
+    }
+    if (input_extension == ".obj")
+    {
+        igl::readOBJ(input.string(), V, F);
+    }
+    if (input_extension == ".stl")
+    {
+        Eigen::MatrixXd N;
+        igl::readSTL(input.string(), V, F, N);
+    }
+
+    Eigen::MatrixXi B{};
+    igl::is_vertex_manifold(F, B);
+    bool const are_edges_manifold = igl::is_edge_manifold(F);
+    std::vector<int> vertex_loop{};
+    igl::boundary_loop(F, vertex_loop);
+
+    std::cout << "Vertices: " << V.rows() << "\n";
+    std::cout << "Faces: " << F.rows() << "\n";
+    std::cout << "Manifold vertices: " << B.array().sum() << "\n";
+    std::cout << "Manifold edges: " << std::boolalpha << are_edges_manifold << "\n";
+    std::cout << "Watertight: " << std::boolalpha << vertex_loop.empty() << "\n";
+
+    using Kernel = CGAL::Simple_cartesian<double>;
+    using SurfaceMesh = CGAL::Polyhedron_3<Kernel, CGAL::Polyhedron_items_with_id_3>;
+
+    SurfaceMesh M{};
+    bool const mesh_to_polyhedron_success = mesh_to_polyhedron(V, F, M);
+
+    std::cout << "Mesh to CGAL polyhedron test: " << mesh_to_polyhedron_success << "\n";
+    std::cout << "CGAL Polyhedron vertices: " << M.size_of_vertices() << "\n";
+    std::cout << "CGAL Polyhedron facets: " << M.size_of_facets() << "\n";
+
+    return 0;
+}
--- surface_from_tet.cpp
+++ surface_from_tet.cpp
@@ -1,15 +1,155 @@
-#include <igl/readMESH.h>
-#include <igl/writeOBJ.h>
-#include <igl/writePLY.h>
-#include <igl/writeOFF.h>
-#include <igl/boundary_facets.h>
-
-#include <iostream>
-#include <string>
+#include <algorithm>
+#include <array>
 #include <filesystem>
 #include <fstream>
+#include <igl/boundary_facets.h>
+#include <igl/readMESH.h>
+#include <igl/writeOBJ.h>
+#include <igl/writeOFF.h>
+#include <igl/writePLY.h>
+#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_set>
+#include <vector>
 
-int main(int argc, char **argv)
+bool verify_tet_mesh_topology(Eigen::MatrixXi const& T)
+{
+    using face_type = std::array<unsigned int, 3u>;
+
+    auto const less = [](face_type const& f1, face_type const& f2) {
+        face_type f1_ = f1;
+        face_type f2_ = f2;
+        std::sort(f1_.begin(), f1_.end());
+        std::sort(f2_.begin(), f2_.end());
+        return f1_ < f2_;
+    };
+
+    std::map<face_type, unsigned int, decltype(less)> face_occurrences(less);
+    for (auto t = 0u; t < T.rows(); ++t)
+    {
+        face_type f1{T(t, 0), T(t, 1), T(t, 3)};
+        face_type f2{T(t, 1), T(t, 2), T(t, 3)};
+        face_type f3{T(t, 2), T(t, 0), T(t, 3)};
+        face_type f4{T(t, 0), T(t, 2), T(t, 1)};
+
+        face_occurrences[f1]++;
+        face_occurrences[f2]++;
+        face_occurrences[f3]++;
+        face_occurrences[f4]++;
+    }
+
+    bool const faces_referenced_once_or_twice = std::all_of(
+        face_occurrences.begin(),
+        face_occurrences.end(),
+        [](std::pair<face_type const, unsigned int> const& kv) {
+            return kv.second == 1u || kv.second == 2u;
+        });
+
+    return faces_referenced_once_or_twice;
+}
+
+bool verify_surface(Eigen::MatrixXi const& F)
+{
+    using edge_type = std::array<unsigned int, 2u>;
+
+    auto const less = [](edge_type const& e1, edge_type const& e2) {
+        edge_type e1_ = e1;
+        edge_type e2_ = e2;
+        std::sort(e1_.begin(), e1_.end());
+        std::sort(e2_.begin(), e2_.end());
+        return e1_ < e2_;
+    };
+
+    std::map<edge_type, unsigned int, decltype(less)> edge_occurrences(less);
+    for (auto f = 0u; f < F.rows(); ++f)
+    {
+        edge_type e1{F(f, 0), F(f, 1)};
+        edge_type e2{F(f, 1), F(f, 2)};
+        edge_type e3{F(f, 2), F(f, 0)};
+
+        edge_occurrences[e1]++;
+        edge_occurrences[e2]++;
+        edge_occurrences[e3]++;
+    }
+
+    bool const edge_referenced_once_or_twice = std::all_of(
+        edge_occurrences.begin(),
+        edge_occurrences.end(),
+        [](std::pair<edge_type const, unsigned int> const& kv) {
+            return kv.second == 1u || kv.second == 2u;
+        });
+
+    return edge_referenced_once_or_twice;
+}
+
+std::vector<int> unreferenced_vertices(Eigen::MatrixXd const& V, Eigen::MatrixXi const& F)
+{
+    std::vector<unsigned int> vertex_occurrences{};
+    vertex_occurrences.resize(V.rows(), 0u);
+    for (auto f = 0u; f < F.rows(); ++f)
+    {
+        for (auto j = 0u; j < 3u; ++j)
+        {
+            vertex_occurrences[F(f, j)]++;
+        }
+    }
+    std::vector<int> unreferencedV{};
+    for (auto i = 0u; i < vertex_occurrences.size(); ++i)
+    {
+        if (vertex_occurrences[i] > 0u)
+            continue;
+
+        unreferencedV.push_back(i);
+    }
+    return unreferencedV;
+}
+
+std::pair<Eigen::MatrixXd, Eigen::MatrixXi> remove_unreferenced_vertices(
+    Eigen::MatrixXd const& V,
+    Eigen::MatrixXi const& F,
+    std::vector<int> const& unreferencedV)
+{
+    std::unordered_set<int> unrefV(unreferencedV.begin(), unreferencedV.end());
+    std::vector<int> prefix_sum{};
+    prefix_sum.reserve(V.rows());
+    int sum = 0;
+    for (auto i = 0u; i < V.rows(); ++i)
+    {
+        auto it = unrefV.find(i);
+        prefix_sum.push_back(sum);
+        if (it == unrefV.end())
+            ++sum;
+    }
+    prefix_sum.push_back(sum);
+
+    Eigen::MatrixXd VF;
+    Eigen::MatrixXi FF;
+    VF.resize(prefix_sum.back(), 3u);
+    FF.resize(F.rows(), F.cols());
+
+    auto const is_removed = [&](int i) {
+        return prefix_sum[i] == prefix_sum[i + 1];
+    };
+    for (auto i = 0u; i < V.rows(); ++i)
+    {
+        if (is_removed(i))
+            continue;
+
+        VF.row(prefix_sum[i]) = V.row(i);
+    }
+    for (auto f = 0u; f < F.rows(); ++f)
+    {
+        for (auto j = 0u; j < 3u; ++j)
+        {
+            FF(f, j) = prefix_sum[F(f, j)];
+        }
+    }
+
+    return {VF, FF};
+}
+
+int main(int argc, char** argv)
 {
     if (argc == 1 || argc != 4)
     {
@@ -20,10 +160,9 @@ int main(int argc, char **argv)
 
     std::filesystem::path const input{argv[1]};
     std::string const input_extension = input.extension().string();
-    bool const is_valid_input_file =
-        std::filesystem::exists(input) &&
-        std::filesystem::is_regular_file(input) &&
-        input_extension == ".mesh";
+    bool const is_valid_input_file    = std::filesystem::exists(input) &&
+                                     std::filesystem::is_regular_file(input) &&
+                                     input_extension == ".mesh";
 
     if (!is_valid_input_file)
     {
@@ -34,13 +173,23 @@ int main(int argc, char **argv)
     Eigen::MatrixXd V;
     Eigen::MatrixXi T, F;
     igl::readMESH(input.string(), V, T, F);
+    T = T.rowwise().reverse().eval();
     F = igl::boundary_facets<Eigen::MatrixXi, Eigen::MatrixXi>(T);
-    Eigen::PermutationMatrix<3> P{};
-    P.indices() = {0, 2, 1};
-    F = F * P;
+    std::vector<int> const unreferencedV = unreferenced_vertices(V, F);
+    std::tie(V, F)                       = remove_unreferenced_vertices(V, F, unreferencedV);
+    F                                    = F.rowwise().reverse().eval();
+
+    bool const does_tet_mesh_have_correct_topology  = verify_tet_mesh_topology(T);
+    bool const does_face_mesh_have_correct_topology = verify_surface(F);
+
+    std::cout << std::boolalpha << "Tet mesh topology: " << does_tet_mesh_have_correct_topology
+              << "\n";
+    std::cout << std::boolalpha << "Surface mesh topology: " << does_face_mesh_have_correct_topology
+              << "\n";
+    std::cout << "Unreferenced vertices: " << unreferencedV.size() << "\n";
 
     std::filesystem::path const path_to_output = argv[2];
-    std::string const output_file_type = argv[3];
+    std::string const output_file_type         = argv[3];
     if (output_file_type == "ply")
     {
         std::filesystem::path const output = path_to_output / (input.stem().string() + ".ply");
